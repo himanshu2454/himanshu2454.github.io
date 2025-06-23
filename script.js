b763ad8b3c0e7a0507aa2a10308ede6ed783b9db
Himanshu Chouhan <hchouhan3654@gmail.com>
2025-06-23 17:52:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll animation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
